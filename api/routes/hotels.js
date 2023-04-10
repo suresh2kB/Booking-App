053a0f0a55fdf3d16e99bf159b6f196271c3ac00
Suresh Kumar <suresh05122000@gmail.com>
2023-04-10 16:12:07 +0530

    Added simple error handling for one method

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,5 +1,6 @@
 import express from "express";
 import Hotel from "../models/Hotel.js";
+import { createError } from "../utils/error.js";
 
 const router = express.Router();
 
@@ -54,12 +55,12 @@ router.get("/:id", async (req, res) => {
 });
 
 // Get ALL hotels.
-router.get("/", async (req, res) => {
+router.get("/", async (req, res, next) => {
   try {
     const hotels = await Hotel.find();
     res.status(200).json(hotels);
   } catch (err) {
-    res.status(500).json(err);
+    next(err);
   }
 });
 
