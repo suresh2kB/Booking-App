17bb391a8b741239094a57dedf0ae29519fcc961
Suresh Kumar <suresh05122000@gmail.com>
2023-04-12 10:59:14 +0530

    Added routes for users

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,5 +1,23 @@
 import express from "express";
+import {
+  updateUser,
+  deleteUser,
+  getUser,
+  getUsers,
+} from "../controller/user.js";
 
 const router = express.Router();
+
+// UPDATE User.
+router.put("/:id", updateUser);
+
+// DELETE User.
+router.delete("/:id", deleteUser);
+
+// GET User.
+router.get("/:id", getUser);
+
+// Get ALL Users.
+router.get("/", getUsers);
 
 export default router;
