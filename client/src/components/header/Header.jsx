9f52c0d7a9c85184def3287b796da640c2a05397
Suresh Kumar <suresh05122000@gmail.com>
2023-04-15 17:54:11 +0530

    Added header

---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import "./Header.css";
+import {
+  faBed,
+  faPlane,
+  faCar,
+  faTaxi,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="headerContainer">
+        <div className="headerList">
+          <div className="headerListItem active">
+            <FontAwesomeIcon icon={faBed} />
+            <span>Stays</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faPlane} />
+            <span>Flights</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faCar} />
+            <span>Car rentals</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faBed} />
+            <span>Attractions</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faTaxi} />
+            <span>Airport Taxis</span>
+          </div>
+        </div>
+        <h1 className="headerTitle">A lifetime of discounts? It's Genius</h1>
+        <p className="headerDisc">
+          Get rewarded for your travel - unlock instant savings of 10% or more
+          with a free Lamabooking account.
+        </p>
+        <button className="headerButton">Sign in / Register</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./Home.css";
 import Navbar from "../../components/navbar/Navbar";
+import Header from "../../components/header/Header";
 const Home = () => {
   return (
     <div>
       <Navbar></Navbar>
+      <Header />
     </div>
   );
 };
